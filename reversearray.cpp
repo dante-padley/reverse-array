9357c89273dcf1ec1d319694c5ab5cb9df565f69
Dante Padley <dante_padley@yahoo.com>
2019-02-07 19:04:24 -0800

    created reverseArray function that accepts an int array and size, creates an array of same size, reads the first array into the second backwards, then reads the second into the first directly.

--- reversearray.cpp
+++ reversearray.cpp
@@ -2,22 +2,47 @@
 
 using namespace std;
 
-int main(){
-//initialize variable for array size
-int size;
-//collect array size from user
-cout << "How many items will be in the array?\n";
-cin >> size;
-//initialize array with size
-int myArray [size];
-cout << "Enter the items one at a time.\n";
-//typical while loop to grab elements from user.
-int i = 0;
-while(i < size){
-cin >> myArray[i];
-i++;
-}
-//need to implement reverseArray function to reverse the array and print it out.
+//global variable size
 
-return 0;
+
+void reverseArray(int myArray[], int fsize){
+  int * p;
+  p = myArray;
+
+  int bArray [fsize];
+  int * b;
+  b = bArray;
+
+  int k = fsize - 1;
+  for(int i=0; i < fsize; i++){
+    *(b+i) = *(p+k);
+    k--;
+  }
+  for(int i=0; i < fsize; i++){
+    *(p+i) = *(b+i);
+  }
+
+}
+int main(){
+
+  int size;
+  //collect array size from user
+  cout << "How many items will be in the array?\n";
+  cin >> size;
+  //initialize array with size
+  int myArray [size];
+  cout << "Enter the items one at a time.\n";
+  //typical while loop to grab elements from user.
+  for(int i=0; i < size; i++){
+    cin >> myArray[i];
+  }
+  //need to implement reverseArray function to reverse the array and print it out.
+  reverseArray(myArray, size);
+
+  cout << "Here is the array after reversing the elements in place.\n";
+  for(int i=0; i < size; i++){
+    cout << myArray[i] << endl;
+
+  }
+  return 0;
 }
